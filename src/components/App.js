488512b3c7f4a0234ea09bf2a14e7a0f9e463312
manisha goyal <48145474+mahigoyal123@users.noreply.github.com>
2021-05-20 17:53:54 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -4,14 +4,34 @@ import '../styles/App.css';
 const App = () => {
   // write your code here 
 
+  const [getCounter, setCounter] = useState();
+
+  useEffect( () => {
+     if(getCounter>0){
+       setTimeout(() => setCounter(getCounter-1), 1000);
+     }
+    
+  }) ;
+
+  const callerKey = (e) => {
+    if(e.key==="Enter"){
+    setCounter(Math.floor(e.target.value));
+    }
+  }
+  
   return (
     <div className="wrapper">
-      <div id="whole-center">
+       <div id="whole-center">
         <h1>
-          Reverse countdown for<input id="timeCount" onKeyDown={/* callback function */} /> sec.
+          Reverse countdown for<input id="timeCount" onKeyDown={callerKey} /> sec.
         </h1>
       </div>
-      <div id="current-time">{/* remaining time */}</div>
+      <div id="current-time">{ getCounter } </div> 
+
+      {/* <input onKeyDown= {callerKey}/>
+     {getCounter} */}
+
+
     </div>
   )
 }
